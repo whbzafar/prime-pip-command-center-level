9f905abf6823ccacd97b53103dfb64515f7f9371
whbzafar <primepipfx13@gmail.com>
2026-09-20 22:20:15 +0500

    fix(community): load hub layout correction

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './community-layout-fix.css';
 import { applyInterfaceTemplate } from './data/interfaceTemplates';
 
 try {
